0d038457f146ff45f94660e511bc4e3784e8b1f5
misho <mihailirinchev@gmail.com>
2014-02-20 00:20:10 +0200

    First problem classes

---
+++ OOPHW5/FirstProblem/Circle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shape
+{
+	internal class Circle : Shape
+	{
+		public Circle(double width)	: base(width, width) 
+		{
+		}
+
+		public override double CalculateSurface()
+		{
+			return Math.PI * this.Height * this.Width;
+		}
+	}
+}
---
+++ OOPHW5/FirstProblem/Shape.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shape
+{
+	internal abstract class Shape
+	{
+		private double width = 1;
+
+		private double height = 1;
+
+		public double Width 
+		{
+			get
+			{
+				return this.width;
+			}
+			set
+			{
+				if (this.width <= 0)
+				{
+					throw new ArgumentOutOfRangeException("Width must be positive number");
+				}
+
+				this.width = value;
+			}
+		}
+
+		public double Height
+		{
+			get
+			{
+				return this.height;
+			}
+			set
+			{
+				if (this.height <= 0)
+				{
+					throw new ArgumentOutOfRangeException("Height must be positive number");
+				}
+
+				this.height = value;
+			}
+		}
+
+		public Shape(double width, double height)
+		{
+			this.Width = width;
+			this.Height = height;
+		}
+
+		public abstract double CalculateSurface();
+	}
+}
