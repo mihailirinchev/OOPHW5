ee2e65ebd812561a5992be9aebb03b3608084621
misho <mihailirinchev@gmail.com>
2014-02-20 00:14:21 +0200

    First problem done

--- OOPHW5/FirstProblem/Program.cs
+++ OOPHW5/FirstProblem/Program.cs
@@ -4,13 +4,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FirstProblem
+namespace Shape
 {
 	class Program
 	{
-		static void Main(string[] args)
+		static void Main()
 		{
-			Console.WriteLine("Hello world!");
+			var circles = new[]
+			{
+				new Circle(2),
+				new Circle(8),
+				new Circle(6),
+				new Circle(7)
+			};
+			var rectangles = new[]
+			{
+				new Rectangle(9,4),
+				new Rectangle(7,6),
+				new Rectangle(3,7),
+				new Rectangle(4,2)
+			};
+			var triangles = new[]
+			{
+				new Triangle(4, 2),
+				new Triangle(6, 5),
+				new Triangle(3, 1),
+				new Triangle(1, 1)
+			};
+
+			Console.WriteLine(string.Format("Area of Circle with radius = {0} is : {1}", circles[1].Height, circles[1].CalculateSurface()));
+			Console.WriteLine(string.Format("Area of Rectangle with height = {0} and width = {1} is : {2}", rectangles[2].Height, rectangles[2].Width, rectangles[2].CalculateSurface()));
+			Console.WriteLine(string.Format("Area of Triangle with height = {0} and width = {1} is : {2}", triangles[2].Height, triangles[2].Width, triangles[2].CalculateSurface()));
 		}
 	}
 }
