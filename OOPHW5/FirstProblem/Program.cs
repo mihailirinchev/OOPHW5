1f307f7b281d539464ac13a4b4d6d09b66045e70
Mihail Irinchev <mihailirinchev@gmail.com>
2014-02-20 20:03:55 +0200

    fixed for the other shapes

--- OOPHW5/FirstProblem/Program.cs
+++ OOPHW5/FirstProblem/Program.cs
@@ -8,29 +8,23 @@ namespace Shape
 	{
 		static void Main()
 		{
-			var circles = new Dictionary<Shape, double>
+			var shapes = new Dictionary<Shape, double>
 			{
 				{ new Circle(2), 12.566370614359172953850573533118 },
 				{ new Circle(8), 201.06192982974676726160917652989 },
 				{ new Circle(6), 113.09733552923255658465516179806 },
 				{ new Circle(7), 153.9380400258998686846695257807 },
-			};
-			var rectangles = new[]
-			{
-				new Rectangle(9,4),
-				new Rectangle(7,6),
-				new Rectangle(3,7),
-				new Rectangle(4,2)
-			};
-			var triangles = new[]
-			{
-				new Triangle(4, 2),
-				new Triangle(6, 5),
-				new Triangle(3, 1),
-				new Triangle(1, 1)
+				{ new Rectangle(9,4), 36 },
+				{ new Rectangle(7,6), 42 },
+				{ new Rectangle(3,7), 21 },
+				{ new Rectangle(4,2), 8 },
+				{ new Triangle(4, 2), 4 },
+				{ new Triangle(6, 5), 15 },
+				{ new Triangle(3, 1), 1.5 },
+				{ new Triangle(1, 1), 0.5 },
 			};
 
-			foreach (var shape in circles)
+			foreach (var shape in shapes)
 			{
 				var surface = shape.Key.CalculateSurface();
 				if (surface - shape.Value > 0.01)
