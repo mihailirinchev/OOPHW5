0d181cfe3f1155b44591ecd8f05d4ee96542d9ec
Mihail Irinchev <mihailirinchev@gmail.com>
2014-02-20 23:22:39 +0200

    Task3

---
+++ OOPHW5/InvalideRangeException/InvalidRangeException.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.Serialization;
+
+namespace InvalidRangeException
+{
+	internal class InvalidRangeException<T> : Exception
+	{
+		private const string DefaultErrorMessageFormat = "The value does not fall within the expected range: [{0}...{1}]";
+
+		public T Start { get; private set; }
+
+		public T End { get; private set; }
+
+		public InvalidRangeException(T start, T end) : this(start, end, string.Format(DefaultErrorMessageFormat, start, end))
+		{
+		}
+
+		public InvalidRangeException(T start, T end, string message) : this(start, end, message, null)
+		{
+		}
+
+		public InvalidRangeException(T start, T end, string message, Exception innerException) : base(message, innerException)
+		{
+			this.Start = start;
+			this.End = end;
+		}
+
+		public InvalidRangeException(SerializationInfo info, StreamingContext context) : base(info, context)
+		{
+		}
+	}
+}
---
+++ OOPHW5/InvalideRangeException/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+
+namespace InvalidRangeException
+{
+	internal class Program
+	{
+		private static readonly DateTime minDate = new DateTime(1980, 1, 1);
+		private static readonly DateTime maxDate = new DateTime(2013,12,31);
+		private static readonly Random rand = new Random();
+
+		static void Main(string[] args)
+		{
+			while (true)
+			{
+				try
+				{
+					if (rand.Next(2) == 0)
+					{
+						AskForAnInt();
+					}
+					else
+					{
+						AskForADateTime();
+					}
+				}
+				catch (Exception ex)
+				{
+					Console.WriteLine("An error occurred: {0}", ex.Message);
+				}
+			}
+		}
+
+		private static void AskForAnInt()
+		{
+			Console.Write("Input integer within [0...100]: ");
+			var val = int.Parse(Console.ReadLine());
+
+			if (val < 0 || val > 100)
+			{
+				throw new InvalidRangeException<int>(0, 100);
+			}
+		}
+
+		private static void AskForADateTime()
+		{
+			Console.Write("Input datetime within [1.1.1980...31.12.2013]: ");
+			var val = DateTime.Parse(Console.ReadLine());
+
+			if (val < minDate || val > maxDate)
+			{
+				throw new InvalidRangeException<DateTime>(minDate, maxDate);
+			}
+		}
+	}
+}
